719c3df5917c8e2778fd8f727033cc2ace82e716
ArdenchikPlus <raskovalov132@gmail.com>
2026-06-05 13:42:23 +0300

    bug fix

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n=input("Enter a list of products:")
+import re
+n = input("Enter a list of products:")
 clean_list = n.strip().lower()
-no_duplicates =  list(set(clean_list))
-words = no_duplicates.sort()
+words_list = clean_list.split(", ")
+no_duplicates = list(set(words_list))
+no_duplicates.sort(key=str.lower)
+
+print(", ".join(no_duplicates))
