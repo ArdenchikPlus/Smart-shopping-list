51907d38bb9a312fa5ddffa48da6b180da8cac79
ArdenchikPlus <raskovalov132@gmail.com>
2026-06-05 13:53:34 +0300

    last version

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ clean_list = n.strip().lower()
 words_list = clean_list.split(", ")
 no_duplicates = list(set(words_list))
 no_duplicates.sort(key=str.lower)
+for i, word in enumerate(no_duplicates, start=1):
+    print(f'{i}. {word}')
 
-print(", ".join(no_duplicates))
